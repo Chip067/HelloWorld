3b70f45820d43c22262957a8f58dec208c1367c5
Chip067 <antti.salmi91@hotmail.com>
2016-11-06 18:08:26 +0200

    Create main

--- HelloWorld/Source.cpp
+++ HelloWorld/Source.cpp
@@ -1,7 +1,18 @@
 /*Main File*/
 #include "HelloWorld.h"
 
+using std::cout;
+using std::endl;
+
 int main() {
 
+	// Create HelloWorld object and set the message
+	HelloWorld hello;
+	hello.SetMessage("Setting the Hello World message!");
+
+	// Print the message	
+	cout << hello.GetMessage() << endl;
+
+	system("pause");
 	return 0;
 }
